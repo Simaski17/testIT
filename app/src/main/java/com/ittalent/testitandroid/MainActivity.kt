08763cc25f29739bfff0e508554a75af196346c8
Jimmy Hernandez <Jimmy Hernandez>
2020-03-17 17:30:17 -0300

    first commit

---
+++ app/src/main/java/com/ittalent/testitandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ittalent.testitandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
